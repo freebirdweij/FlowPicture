d6b8067680c638f41b19ff9db9c756aba2878ecf
freebirdweij <freebirdweij@foxmail.com>
2014-02-12 21:25:39 +0800

    u

--- src/com/weij/pic/folwpicture/MainActivity.java
+++ src/com/weij/pic/folwpicture/MainActivity.java
@@ -13,6 +13,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
@@ -22,20 +25,21 @@ import android.widget.HorizontalScrollView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.RadioGroup.LayoutParams;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.origamilabs.library.views.StaggeredGridView;
 import com.weij.pic.flowpicture.R;
 import com.weij.pic.flowpicture.views.CircleViewFlow;
 import com.weij.pic.flowpicture.views.ImageViewFlipper;
 
-public class MainActivity extends Activity implements OnTouchListener{
+public class MainActivity extends Activity implements OnTouchListener,OnGestureListener{
 
 	int time = 5;
 	private StaggeredGridView gview;
 	public String[] urls;
 	public Integer[] imags;
 	public ArrayList<Integer> imaglist = new ArrayList<Integer>();
-
+	private GestureDetector mGestureDetector;  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -51,6 +55,13 @@ public class MainActivity extends Activity implements OnTouchListener{
 		//HorizontalScrollView hview = (HorizontalScrollView) gview.getParent().getParent();
 		//hview.setOnTouchListener(this);
 		gview.setOnTouchListener(this);
+		gview.setFocusable(true);  
+		  
+		gview.setClickable(true);  
+  
+		gview.setLongClickable(true);  
+  
+        mGestureDetector.setIsLongpressEnabled(true);
 
 		Field[] fields = R.drawable.class.getDeclaredFields();
 		List<String> list = new ArrayList<String>();
@@ -110,6 +121,11 @@ public class MainActivity extends Activity implements OnTouchListener{
 
 	}
 
+	public MainActivity() {
+		mGestureDetector = new GestureDetector(this);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		//getMenuInflater().inflate(R.menu.activity_main, menu);
@@ -134,7 +150,111 @@ public class MainActivity extends Activity implements OnTouchListener{
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		// TODO Auto-generated method stub
-		return false;
+		return mGestureDetector.onTouchEvent(event);
+	}
+
+	// 用户轻触触摸屏，由1个MotionEvent ACTION_DOWN触发 
+	@Override
+	public boolean onDown(MotionEvent e) {
+		// TODO Auto-generated method stub
+		Log.i("MyGesture", "onDown");  
+		  
+        Toast.makeText(this, "onDown", Toast.LENGTH_SHORT).show();  
+  
+        return true;  
+	}
+
+	// 用户按下触摸屏、快速移动后松开，由1个MotionEvent ACTION_DOWN, 多个ACTION_MOVE, 1个ACTION_UP触发 
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		// 参数解释：  
+		  
+        // e1：第1个ACTION_DOWN MotionEvent  
+  
+        // e2：最后一个ACTION_MOVE MotionEvent  
+  
+        // velocityX：X轴上的移动速度，像素/秒  
+  
+        // velocityY：Y轴上的移动速度，像素/秒  
+  
+        // 触发条件 ：  
+  
+        // X轴的坐标位移大于FLING_MIN_DISTANCE，且移动速度大于FLING_MIN_VELOCITY个像素/秒  
+  
+        final int FLING_MIN_DISTANCE = 100, FLING_MIN_VELOCITY = 200;  
+  
+        if (e1.getX() - e2.getX() > FLING_MIN_DISTANCE  
+                && Math.abs(velocityX) > FLING_MIN_VELOCITY)  
+  
+        {  
+  
+            // Fling left  
+  
+            Log.i("MyGesture", "Fling left");  
+  
+            Toast.makeText(this, "Fling Left", Toast.LENGTH_SHORT).show();  
+  
+        }  
+  
+        else if (e2.getX() - e1.getX() > FLING_MIN_DISTANCE  
+                && Math.abs(velocityX) > FLING_MIN_VELOCITY)  
+  
+        {  
+  
+            // Fling right  
+  
+            Log.i("MyGesture", "Fling right");  
+  
+            Toast.makeText(this, "Fling Right", Toast.LENGTH_SHORT).show();  
+  
+        }  
+  
+        return false;  
+	}
+
+	 // 用户长按触摸屏，由多个MotionEvent ACTION_DOWN触发 
+	@Override
+	public void onLongPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+		Log.i("MyGesture", "onLongPress");  
+		  
+        Toast.makeText(this, "onLongPress", Toast.LENGTH_LONG).show();  
+	}
+
+	// 用户按下触摸屏，并拖动，由1个MotionEvent ACTION_DOWN, 多个ACTION_MOVE触发 
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		// TODO Auto-generated method stub
+		 Log.i("MyGesture", "onScroll");  
+		  
+	        Toast.makeText(this, "onScroll", Toast.LENGTH_LONG).show();  
+	  
+	        return true; 
+	}
+
+	/*        
+     * * 用户轻触触摸屏，尚未松开或拖动，由一个1个MotionEvent ACTION_DOWN触发 * 
+     * 注意和onDown()的区别，强调的是没有松开或者拖动的状态 
+     */ 
+	@Override
+	public void onShowPress(MotionEvent e) {
+		 Log.i("MyGesture", "onShowPress");  
+		  
+	        Toast.makeText(this, "onShowPress", Toast.LENGTH_SHORT).show();  
+	}
+
+	// 用户（轻触触摸屏后）松开，由一个1个MotionEvent ACTION_UP触发 
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		// TODO Auto-generated method stub
+		Log.i("MyGesture", "onSingleTapUp");  
+		  
+        Toast.makeText(this, "onSingleTapUp", Toast.LENGTH_SHORT).show();  
+  
+        return true;
 	}
 
 
